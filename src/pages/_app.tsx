61b33d03ef9230e15d8b330e84cb84bdb6a7cee3
Gustavo Fraga <gustavohenriquemiele@live.com>
2021-01-10 09:00:27 -0300

    fundamentos e lidando com dados, ssr e ssg

---
+++ src/pages/_app.tsx
@@ -0,0 +1,12 @@
+import GlobalStyle from "../styles/GlobalStyle";
+import { Title } from "../styles/pages/Home";
+
+export default function MyApp({ Component, pageProps }) {
+  return (
+    <>
+      <GlobalStyle />
+      <Component {...pageProps} />
+    </>
+  )
+}
+
